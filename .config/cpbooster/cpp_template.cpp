ca65da80bee617d04f828abff8005a578ce74fc3
Raman Preet Singh <ramanpre0810@gmail.com>
2022-02-19 20:03:14 +0530

    Initial Change

---
+++ .config/cpbooster/cpp_template.cpp
@@ -0,0 +1,18 @@
+#include <cstdint>
+#ifdef ONPC
+#define _GLIBCXX_DEBUG
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+int32_t main() {
+  // ios:sync_with_stdio(0);
+  cin.tie(0);
+
+
+}
