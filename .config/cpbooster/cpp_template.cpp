96b92e029ed717fe5747c668a98aa1f0aa494592
Raman Preet Singh <ramanpre0810@gmail.com>
2022-06-18 20:14:36 +0530

    Fixed cpp_template

--- .config/cpbooster/cpp_template.cpp
+++ .config/cpbooster/cpp_template.cpp
@@ -10,7 +10,7 @@ typedef long long ll;
 typedef long double ld;
 
 int32_t main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.tie(0);
 }
